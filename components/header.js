1f87589d039eca95f0d40c39ea52243f8722585e
abdullahbq <abdullahbinqueyam@gmail.com>
2024-01-10 12:36:52 +0530

    Update Header

--- components/header.js
+++ components/header.js
@@ -11,7 +11,7 @@ class HeaderComponent extends HTMLElement {
       this.innerHTML = `
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
               <div class="container-fluid">
-                  <a class="navbar-brand" href="#">Your Brand</a>
+                  <a class="navbar-brand" href="#">Reusable_HTML by abdullahbq</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                   </button>
